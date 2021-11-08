43f3ffe8a6879f2174839677565e0eda2501ed18
Myles Nathaniel Lister <mylesnlister@Myless-MacBook-Pro.local>
2021-11-08 14:39:15 -0500

    Finally have the backend API running with the corsheader package installed

--- music_library/music_library/settings.py
+++ music_library/music_library/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corheaders.middleware.CorsMiddleware'
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'music_library.urls'
